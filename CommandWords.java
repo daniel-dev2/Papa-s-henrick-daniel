dbfd214d8956bf664c4cc6cfb5b63aaea0ee91af
Daniel <110735335+daniel-dev2@users.noreply.github.com>
2023-08-30 15:05:59 -0300

    Update CommandWords.java
    adicionei comando olhar

--- CommandWords.java
+++ CommandWords.java
@@ -13,7 +13,7 @@ public class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String[] validCommands = {
-        "ir", "sair", "ajuda", "voltar"
+        "ir", "sair", "ajuda", "voltar", "olhar"
     };
 
     /**
